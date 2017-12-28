941cb5934529199e8823fae26ef9a6ba69352e5b
juagu72 <34918585+juagu72@users.noreply.github.com>
2017-12-28 19:26:59 +0100

    Update hellojuri.cpp

--- HelloJuri/hellojuri.cpp
+++ HelloJuri/hellojuri.cpp
@@ -16,5 +16,5 @@ HelloJuri::~HelloJuri()
 void HelloJuri::on_pushButton_clicked()
 {
 //	QMessageBox::aboutQt(this);
-	QMessageBox::about(this, "Hallo Juri", "Version 0.1");
+	QMessageBox::about(this, "Hallo Juri", "Version 0.2");
 }
